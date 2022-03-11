d02f250b88e9d4e449ec7ffa43126e0c930f19be
Billy Knight <8345283+BillyKoala@users.noreply.github.com>
2022-03-11 20:37:32 +0000

    Update Minimum Denominations.py

--- Minimum Denominations.py
+++ Minimum Denominations.py
@@ -1,6 +1,6 @@
-
 """
 Billy Knight - January 2022
+
 This module will calculate the minimum denominations
 required for change given. All UK denominations are included.
 Decimal points are removed to streamline the processing.
@@ -18,23 +18,23 @@ denomination = 0
 remainder = 0
 
 change = input("Please Enter Your Change Amount: ")
+"""
+Preserve the original user input for 
+the header in the 'change_given' array.
+"""
+static_change = change
 
 # If a decimal point has been input, remove it. We are only using pence from here.
 change = change.replace(".", "") if "." in str(change) else change
 
 if change.isnumeric():
-    """
-    Preserve the original user input for 
-    the header in the 'change_given' array.
-    """
-    static_change = change
 
     change = int(change)
 
-    if change < 100: # We are managing change denominations of less than £1 here.
+    if change < 100:  # We are managing change denominations of less than £1 here.
         try:
             # Append a header to the change_given array.
-            change_given.append("Minimum denominations for " + static_change + "p change.\n")
+            change_given.append(f"Minimum denominations for " + static_change + "p change.\n")
 
             # Only use the pence values in the denominations array (50p - 1p).
             for i in range(5, len(denominations)):
@@ -42,10 +42,9 @@ if change.isnumeric():
                 quantity = int((change / denominations[i])) if (int(change / denominations[i])) > 0 else 0
 
                 if change < 100 and quantity > 0:
-
                     remainder = change % denominations[i]
                     change -= quantity * denominations[i]
-                    change_given.append(str(denominations[i]) + "p x " + str(quantity))
+                    change_given.append(f"{str(denominations[i])}p x {str(quantity)}")
 
             # Output the array data.
             for i in range(len(change_given)):
@@ -56,10 +55,10 @@ if change.isnumeric():
         finally:
             pass
 
-    else: # We are managing change denominations >= £1 here.
+    else:  # We are managing change denominations >= £1 here.
         try:
             # Append a header to the change_given array.
-            change_given.append("Minimum denominations for £" + static_change + " change.\n")
+            change_given.append(f"Minimum denominations for £{static_change} change\n")
 
             # Use all UK denominations in the denominations array (£50 - 1p)
             for i in range(len(denominations)):
@@ -70,11 +69,11 @@ if change.isnumeric():
                     remainder = change % denominations[i]
                     change -= quantity * denominations[i]
 
-                    if denominations[i] >= 100:# Manage the pound notes here.
-                        change_given.append("£" + str(int(denominations[i]/100)) + " x " + str(quantity))
+                    if denominations[i] >= 100:  # Manage the pound notes here.
+                        change_given.append(f"£{int(denominations[i] / 100)} x {quantity}")
 
-                    else:# Manage the pennies here.
-                        change_given.append(str(denominations[i]) + "p x " + str(quantity))
+                    else:  # Manage the pennies here.
+                        change_given.append(f"{denominations[i]}p x {quantity}")
 
             # Output the array data.
             for i in range(len(change_given)):
@@ -83,4 +82,4 @@ if change.isnumeric():
         finally:
             pass
 else:
-    messagebox.showerror("User Error","Please Use Numbers ONLY.\n\nThank You.")
+    messagebox.showerror("User Error", "Please Use Numbers ONLY.\n\nThank You.")
